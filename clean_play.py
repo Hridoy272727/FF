ed229491705ea6942a53d61620922b4ce88e8968
Hridoy272727 <hpbiswas123@gmail.com>
2025-07-26 22:35:29 +0600

    Add files via upload

---
+++ clean_play.py
@@ -0,0 +1,35 @@
+import requests, json, os, sys, random
+#-------------------------------Warna---------------------------------------------------
+qu = '\033[0;36m'
+hi = '\033[0;32m'
+pu = '\033[0;37m'
+me = '\033[0;31m'
+ku = '\033[0;33m'
+#--------------------------------Penkodisian-------------------------------------------
+def sukses(no1,pro,nam):
+  print("     %s[%s%s%s] [%s Sent %s] %sSuccess, mulai %s from %s%s %ssended" % (pu,ku,no1,pu,hi,pu,pu,pro,ku,nam,hi))
+def gagal(no1,pro,nam):
+  print("     %s[%s%s%s] [%s Failed  %s] %sFailed, mulai %s from %s%s %snot sended" % (pu,ku,no1,pu,me,pu,pu,pro,ku,nam,me))
+#--------------------------------MAIN---------------------------------------------------
+def main():
+  print("%s[%s!%s] %sTarget locked >> %s%s" % (pu,me,pu,pu,ku,"+62"+nom))
+  t = 1
+  for spam in range(jum):
+   print("%s[%s+%s]-------------------------->>>[%s%s%s]<<<--------------------------[%s+%s]" % (pu,ku,pu,me,t,pu,ku,pu))
+   t += 1
+   asakita();sunchila();nutriclub();asani();wintershop();datesy();thaifriendly();jumpstart();kinimart();klikwa();bakmikeraton();kopidulukala();kredinesia();pinjamindo();uangpintar();danafix();maucash();omamoriexpress();ktakilat();cairin();kredito();kreditpedia();bocil();duitqu();primacash();temanprima();maripinjam();sobatbangun()
+#--------------------------------Banner/LOGO------------------------------------------
+def logo():
+  print("""%s
+ _________                    _________
+|   _____/                   |   _____/
+|   |_____ ________________  |   |______ __ _________
+|    ___| ' __|  ___|  ___|  |    ___|  |  '__|  ___|
+|   |   |  |  |  __|   __|   |   |   |  |  |  |  __|
+|___|   |__|  |_____|_____|  |___|   |__|__|  |_____|
+
+%sAuthor  : %sTokicoy%s
+%sGithub  : %sNothing%s
+%sYoutube : %sAcinakongames
+
+%sTools Heck Use ID Free Fire""" % (qu,pu,ku,qu,pu,ku,qu,pu,ku,qu,qu))
